50d881003633fff713044e82a1a85cef80868259
zys37 <franeksiek@gmail.com>
2024-07-19 00:55:29 +0200

    character moving fix and enemies following the player
    title

---
+++ Eye_Enemy.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = get_parent().get_node("Hero").position +  Vector2(1000, 0).rotated(randf_range(0, 2*PI))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	move_and_collide((get_parent().get_node("Hero").position - position).normalized()*2)
+	
--- player.gd
+++ player.gd
@@ -27,14 +27,25 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		is_playing = 1
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var direction = Vector2()
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	elif Input.is_action_pressed("ui_down"):
+		direction.y = 1
+
 	if Input.is_action_pressed("ui_left"):
+		direction.x = -1
 		$Hero.flip_h = true
-	if Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right"):
+		direction.x = 1
 		$Hero.flip_h = false
+
+	if direction.length() > 0:
+		direction = direction.normalized()
+		is_playing = 1
+		velocity = direction * SPEED
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		is_playing = 0
 	move_and_slide()
