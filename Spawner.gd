f47017fa8e8d3ec417872d69e2512b66b7e84a4f
zys37 <franeksiek@gmail.com>
2024-07-20 02:07:37 +0200

    mobs spawning
    added mob spawning and following the player, preparing to add combat

---
+++ Spawner.gd
@@ -0,0 +1,8 @@
+extends Node2D
+var enemy1=preload("res://Eye_Enemy.tscn")
+var enemy2=preload("res://tentacle_enemy.tscn")
+var enemy3=preload("res://weird_enemy.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func spawn():
+	
---
+++ world.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func spawn_mob():
+	var new_mob = preload("res://Eye_Enemy.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf_range(0,1)
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+
+func _on_timer_timeout():
+	spawn_mob()
+	print(%PathFollow2D.global_position)
